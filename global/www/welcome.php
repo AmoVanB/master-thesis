c0e9d1b849a956d18b6e6203475fca4b561db416
Amaury Van Bemten <amaury@vanbemten.com>
2015-04-25 13:46:18 +0200

    Global/local -> centralized/decentralized

--- global/www/welcome.php
+++
@@ -1,5 +0,0 @@
-<p>This interface is part of my master thesis "Using Service Discovery to Apply Policies in Networks" submitted in partial fulfillment of the requirements for the degree of Master in Computer Science and Engineering at the University of Liège (2014-2015).</p>
-
-<p>This interface allows to apply policies in the networks using the <code>service-discovery-daemon</code>. The document containing the exact description of the system is available <a href="">here</a>. The entire work's archive is, for its part, available <a href="">here</a>.</p>
-
-<p class="text-right"><em>Amaury Van Bemten</em></p>
